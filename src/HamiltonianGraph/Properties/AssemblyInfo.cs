cd064cb80da2e3b1846f8c33a5e839c36ea90c03
Sagid, Home(w) <kofon95@mail.ru>
2017-12-31 23:42:19 +0300

    1st release on nuget

--- src/HamiltonianGraph/Properties/AssemblyInfo.cs
+++ src/HamiltonianGraph/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("HamiltonianGraph")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Searches a Hamiltonian cycle")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("HamiltonianGraph")]

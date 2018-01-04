c68fae28bb8cc6eb84ecb1c27ac1f22b228982e7
Sagid, Home(w) <kofon95@mail.ru>
2018-01-04 17:51:19 +0300

    travis init

---
+++ remove_bin_and_obj.sh
@@ -0,0 +1,10 @@
+
+# get around this bug in vs: https://github.com/dotnet/sdk/issues/423
+rm -r src/HamiltonianGraph/bin
+rm -r src/HamiltonianGraph/obj
+rm -r tests/HamiltonianGraph.GraphInputProvider/bin
+rm -r tests/HamiltonianGraph.GraphInputProvider/obj
+rm -r tests/HamiltonianGraph.UnitTests/bin
+rm -r tests/HamiltonianGraph.UnitTests/obj
+rm -r tests/MatchingExtensions/bin
+rm -r tests/MatchingExtensions/obj

39946fe5dca8b3f9c0d3aa8a9f88d754ba7a1c39
Sagid, Home(w) <kofon95@mail.ru>
2018-04-28 16:50:52 +0300

    Add Benchmark project

---
+++ HamiltonianGraph.Benchmark/MainWindow.xaml.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HamiltonianGraph.Benchmark
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+
+        static Random rand = new Random(42);
+
+
+        #region Ranges
+
+        private int _vertexMinumum = 2;
+        public int VertexMinimum
+        {
+            get => _vertexMinumum;
+            set
+            {
+                if (value < 1) return;
+                if (value > VertexMaximum)
+                    SetField(ref _vertexMaximum, value, nameof(VertexMaximum));
+                SetField(ref _vertexMinumum, value);
+            }
+        }
+
+        private int _vertexMaximum = 10;
+        public int VertexMaximum
+        {
+            get => _vertexMaximum;
+            set
+            {
+                if (value < VertexMinimum)
+                    SetField(ref _vertexMinumum, value, nameof(VertexMinimum));
+                SetField(ref _vertexMaximum, value);
+            }
+        }
+
+        private int _weightMinumum = 1;
+        public int WeightMinimum
+        {
+            get => _weightMinumum;
+            set
+            {
+                if (value < 1) return;
+                if (value > WeightMaximum)
+                    SetField(ref _weightMaximum, value, nameof(WeightMaximum));
+                SetField(ref _weightMinumum, value);
+            }
+        }
+
+        private int _weightMaximum = 1000;
+        public int WeightMaximum
+        {
+            get => _weightMaximum;
+            set
+            {
+                if (value < WeightMinimum)
+                    SetField(ref _weightMinumum, value, nameof(WeightMinimum));
+                SetField(ref _weightMaximum, value);
+            }
+        }
+
+
+        private double _density = 1;
+        public double Density
+        {
+            get => _density;
+            set
+            {
+                //if (value > 1 || value < 0) return;
+                SetField(ref _density, value);
+            }
+        }
+
+        private int _graphCount = 1;
+        public int GraphCount
+        {
+            get => _graphCount;
+            set
+            {
+                if (value < 1 || value > 1000) return;
+                SetField(ref _graphCount, value);
+            }
+        }
+
+
+        #endregion
+
+        private int?[,] GetRandomWeights()
+        {
+            int n = rand.Next(VertexMinimum, VertexMaximum + 1);
+            int?[,] weights = new int?[n, n];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i != j && rand.NextDouble() < Density)
+                        weights[i, j] = rand.Next(WeightMinimum, WeightMaximum + 1);
+                }
+            }
+            return weights;
+        }
+
+        private void GenerateGraphs(object sender, RoutedEventArgs e)
+        {
+            var bunchOfWeights = new int?[_graphCount][,];
+            for (int i = 0; i < bunchOfWeights.Length; i++)
+            {
+                bunchOfWeights[i] = GetRandomWeights();
+            }
+            new RunGraphWindow(bunchOfWeights).Show();
+        }
+
+
+        private void SetField<T>(ref T field, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (Equals(field, value)) return;
+            Console.WriteLine($"{propertyName} ({field}) = {value}");
+            field = value;
+            OnPropertyChanged(propertyName);
+        }
+        void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ HamiltonianGraph.Benchmark/RunGraphWindow.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HamiltonianGraph.Benchmark
+{
+    /// <summary>
+    /// Interaction logic for RunGraphWindow.xaml
+    /// </summary>
+    public partial class RunGraphWindow : Window, INotifyPropertyChanged
+    {
+        int?[][,] _weights;
+        // (iterate<foundCycle>, start, end<elapsed>)
+        Action<Action<int[]>, Action, Action<TimeSpan>> findCyclesByBaB;
+        Action<Action<int[]>, Action, Action<TimeSpan>> findCyclesByLC;
+        public string BaBOutput { get => _baBOutput; set => SetField(ref _baBOutput, value); }
+        public string LCOutput { get => _lcOutput; set => SetField(ref _lcOutput, value); }
+        public string BaBResult { get => _baBResult; set => SetField(ref _baBResult, value); }
+        public string LCResult { get => _lcResult; set => SetField(ref _lcResult, value); }
+        public RunGraphWindow(int?[][,] bunchOfWeights)
+        {
+            InitializeComponent();
+            _weights = bunchOfWeights;
+            DataContext = this;
+
+            findCyclesByBaB = (onCycleFound, onStart, onEnd) =>
+            {
+                var sw = new Stopwatch();
+                sw.Start();
+                for (int i = 0; i < bunchOfWeights.Length; i++)
+                {
+                    var cycle = new BranchAndBound(bunchOfWeights[i])
+                        .GetShortestHamiltonianCycle();
+                    if (i == 0)
+                        Dispatcher.Invoke(onStart);
+                    Dispatcher.BeginInvoke(onCycleFound, cycle);
+                }
+                Dispatcher.BeginInvoke(onEnd, sw.Elapsed);
+            };
+
+            findCyclesByLC = (onCycleFound, onStart, onEnd) =>
+            {
+                var sw = new Stopwatch();
+                sw.Start();
+                for (int i = 0; i < bunchOfWeights.Length; i++)
+                {
+                    var cycle = new LatinComposition(bunchOfWeights[i])
+                        .GetShortestHamiltonianCycle();
+                    if (i == 0)
+                        Dispatcher.Invoke(onStart);
+                    onCycleFound(cycle);
+                }
+                Dispatcher.BeginInvoke(onEnd, sw.Elapsed);
+            };
+
+            Closed += (s, e) =>
+            {
+                if (babThread != null && babThread.IsAlive)
+                    babThread.Abort();
+                if (lcThread != null && lcThread.IsAlive)
+                    lcThread.Abort();
+            };
+        }
+
+
+        private void SetField<T>(ref T field, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (Equals(field, value)) return;
+            field = value;
+            OnPropertyChanged(propertyName);
+        }
+        void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        Thread babThread;
+        Thread lcThread;
+        private string _baBOutput;
+        private string _lcOutput;
+        private string _baBResult;
+        private string _lcResult;
+
+        private void RunBaB(object sender, RoutedEventArgs e)
+        {
+            if (babThread != null && babThread.IsAlive)
+                return;
+            BaBOutput = "Ждите...";
+            babThread = new Thread(() =>
+            {
+                findCyclesByBaB(cycle =>
+                {
+                    var path = cycle == null ? "[Путь не найден]" : string.Join("->", cycle);
+                    BaBOutput += path + "\n";
+                }, () => BaBOutput = BaBResult = "", elapsed => BaBResult = elapsed.ToString());
+            });
+            babThread.IsBackground = true;
+            babThread.Start();
+        }
+        private void RunLC(object sender, RoutedEventArgs e)
+        {
+            if (lcThread != null && lcThread.IsAlive)
+                return;
+            LCOutput = "Ждите...";
+            lcThread = new Thread(() =>
+            {
+                findCyclesByLC(cycle =>
+                {
+                    var path = cycle == null ? "[Путь не найден]" : string.Join("->", cycle);
+                    LCOutput += path + "\n";
+                }, () => LCOutput = LCResult = "", elapsed => LCResult = elapsed.ToString());
+            });
+            lcThread.IsBackground = true;
+            lcThread.Start();
+        }
+    }
+}
